ce8b71dac0bf7ee4b93d1036d35091d1f2a8f407
Myst1024 <gunnar.carlson@gmail.com>
2023-11-14 22:52:27 -0600

    fix: use correct db method

--- index.js
+++ index.js
@@ -6,18 +6,21 @@ const serviceAccount = require('./firebaseAccountKey.json');
 
 const user = process.env.user;
 
+const maxTimestampDifference = 3 * 60 * 60 * 1000;
+
 initializeApp({
     credential: cert(serviceAccount)
 });
 
 const db = getFirestore();
 
-
+//!TODO: if user time within timedifference, set to 1 day prior
 const setTime = async () => {
-    await db.collection('hyu').doc('users').set({ [user]: Date.now() })
+    await db.collection('hyu').doc('users').update({ [user]: Date.now() })
 
     const res = await db.collection('hyu').doc('users').get();
-    console.log(res.data())
+    const { gunnar, shay } = res.data();
+    console.log(Math.abs(gunnar - shay));
 
 }
 
