1880cfac07ad1b730f09098c5efc7d2d4dbb8edf
Myst1024 <gunnar.carlson@gmail.com>
2023-11-17 17:46:38 -0600

    feat: added blue/green LEDs, eslint

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ require("dotenv").config();
 const { initializeApp, cert } = require("firebase-admin/app");
 const { getFirestore } = require("firebase-admin/firestore");
 const serviceAccount = require("./firebaseAccountKey.json");
-// For testing only
-const readline = require("node:readline");
-const { stdin: input, stdout: output } = require("node:process");
+const Gpio = require("onoff").Gpio;
 
-const rl = readline.createInterface({ input, output });
+// For testing only
+// const readline = require("node:readline");
+// const { stdin: input, stdout: output } = require("node:process");
+
+// const rl = readline.createInterface({ input, output });
 // For testing only
 
 initializeApp({
@@ -19,6 +21,14 @@ const userLocal = process.env.userLocal;
 const userRemote = process.env.userRemote;
 
 const maxTimestampDifference = 3 * 60 * 60 * 1000;
+const buttonDebounceTimeout = 500;
+
+/** Buttons & LEDs */
+const pushButton = new Gpio(16, "in", "both", {
+  debounceTimeout: buttonDebounceTimeout,
+});
+const ledBlue = new Gpio(23, "out");
+const ledGreen = new Gpio(25, "out");
 
 const withinMaxDifference = (first, second) => {
   return Math.abs(first - second) < maxTimestampDifference;
@@ -35,20 +45,26 @@ const clearLightOffTimeout = () => {
 
 const lightFull = () => {
   console.log("turning on full 🌕");
+  ledBlue.writeSync(0);
+  ledGreen.writeSync(1);
   clearLightOffTimeout();
   lightOffTimeoutID = setTimeout(lightOff, maxTimestampDifference);
 };
 
 const lightHalf = () => {
   console.log("turning on half  🌗");
+  ledGreen.writeSync(0);
+  ledBlue.writeSync(1);
   clearLightOffTimeout();
   lightOffTimeoutID = setTimeout(lightOff, maxTimestampDifference);
 };
 
-function lightOff() {
-  clearLightOffTimeout();
+const lightOff = () => {
   console.log("turning off 🌑");
-}
+  ledGreen.writeSync(0);
+  ledBlue.writeSync(0);
+  clearLightOffTimeout();
+};
 
 let currentData;
 let timeLocal;
@@ -88,16 +104,40 @@ const setLocalUserTime = async (time) => {
 };
 
 // For testing only
-rl.prompt();
-rl.on("line", (input) => {
-  console.log(`Pressed!`);
-  if (withinMaxDifference(Date.now(), timeLocal)) {
-    const yesterday = Date.now() - 24 * 60 * 60 * 1000;
-    setLocalUserTime(yesterday);
-  } else {
-    setLocalUserTime(Date.now());
-  }
-});
+// rl.prompt();
+// rl.on("line", (input) => {
+//   console.log(`Pressed!`);
+//   if (withinMaxDifference(Date.now(), timeLocal)) {
+//     const yesterday = Date.now() - 24 * 60 * 60 * 1000;
+//     setLocalUserTime(yesterday);
+//   } else {
+//     setLocalUserTime(Date.now());
+//   }
+// });
 // For testing only
 
+if (Gpio.accessible) {
+  pushButton.watch(function (err) {
+    if (err) {
+      console.error("There was an error", err);
+      return;
+    }
+    console.log(`Pressed!`);
+    if (withinMaxDifference(Date.now(), timeLocal)) {
+      const yesterday = Date.now() - 24 * 60 * 60 * 1000;
+      setLocalUserTime(yesterday);
+    } else {
+      setLocalUserTime(Date.now());
+    }
+  });
+} else {
+  console.log("gpio not acessible");
+}
+
 watchUsersDoc();
+
+process.on("beforeExit", lightOff);
+process.on("SIGINT", () => {
+  lightOff();
+  process.exit();
+});
