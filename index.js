9dee2f283efb61ade0f521c20efbb23bb9631df7
Myst1024 <gunnar.carlson@gmail.com>
2023-12-02 11:41:16 -0600

    feat: fixed pinout for orangePi

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-require("dotenv").config();
 const { initializeApp, cert } = require("firebase-admin/app");
 const { getFirestore } = require("firebase-admin/firestore");
+const { userLocal, userRemote } = require("./users.json");
 const serviceAccount = require("./firebaseAccountKey.json");
 const Gpio = require("onoff").Gpio;
 
@@ -17,18 +17,16 @@ initializeApp({
 
 const db = getFirestore();
 
-const userLocal = process.env.userLocal;
-const userRemote = process.env.userRemote;
-
 const maxTimestampDifference = 3 * 60 * 60 * 1000;
-const buttonDebounceTimeout = 500;
 
 /** Buttons & LEDs */
-const pushButton = new Gpio(16, "in", "both", {
-  debounceTimeout: buttonDebounceTimeout,
+const pushButton = new Gpio(258, "in", "falling", {
+  debounceTimeout: 50,
+  activeLow: true,
+  reconfigureDirection: false,
 });
-const ledBlue = new Gpio(23, "out");
-const ledGreen = new Gpio(25, "out");
+const ledBlue = new Gpio(260, "out");
+const ledGreen = new Gpio(259, "out");
 
 const withinMaxDifference = (first, second) => {
   return Math.abs(first - second) < maxTimestampDifference;
@@ -116,28 +114,32 @@ const setLocalUserTime = async (time) => {
 // });
 // For testing only
 
-if (Gpio.accessible) {
-  pushButton.watch(function (err) {
-    if (err) {
-      console.error("There was an error", err);
-      return;
-    }
-    console.log(`Pressed!`);
-    if (withinMaxDifference(Date.now(), timeLocal)) {
-      const yesterday = Date.now() - 24 * 60 * 60 * 1000;
-      setLocalUserTime(yesterday);
-    } else {
-      setLocalUserTime(Date.now());
-    }
-  });
-} else {
-  console.log("gpio not acessible");
-}
-
+setTimeout(() => {
+  if (Gpio.accessible) {
+    pushButton.watch(function (err, value) {
+      if (err) {
+        console.error("There was an error", err);
+        return;
+      }
+      console.log(`Pressed!`, value);
+      if (withinMaxDifference(Date.now(), timeLocal)) {
+        const yesterday = Date.now() - 24 * 60 * 60 * 1000;
+        setLocalUserTime(yesterday);
+      } else {
+        setLocalUserTime(Date.now());
+      }
+    });
+  } else {
+    console.log("gpio not acessible");
+  }
+}, 1000);
 watchUsersDoc();
 
 process.on("beforeExit", lightOff);
 process.on("SIGINT", () => {
   lightOff();
+  pushButton.unexport();
+  ledBlue.unexport();
+  ledGreen.unexport();
   process.exit();
 });
